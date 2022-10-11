08d5d984930beeb62abd1df601da8fb2e3186a58
Michio Oguro <michio_oguro@hotmail.com>
2022-10-12 01:04:27 +0900

    docs: Add comment.

--- log.py
+++ log.py
@@ -5,6 +5,14 @@ from typing import Union
 
 class LogFile():
 
+    """
+    Properties:
+        log_file_path (Path):
+            Path of the log file.
+        messages (dict):
+            Messages in the log file.
+    """
+
     def __init__(self, log_file_path: Union[str, Path]) -> None:
         self.log_file_path = Path(log_file_path)
         self._load()
